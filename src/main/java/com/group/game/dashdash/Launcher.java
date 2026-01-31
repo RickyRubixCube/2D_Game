7c444bcc3e9884a221df5ca5e6831b2ed7f1b486
Minkaln <sokhadaravichhey@gmail.com>
2026-01-31 21:27:55 +0700

    update

--- src/main/java/com/group/game/dashdash/Launcher.java
+++ src/main/java/com/group/game/dashdash/Launcher.java
@@ -5,7 +5,6 @@ import com.almasb.fxgl.app.GameApplication;
 public class Launcher
 {
     public static void main(String[] args) {
-        GameApplication.launch(HelloApplication.class, args);
         GameApplication.launch(GGApplication.class, args);
     }
 }
